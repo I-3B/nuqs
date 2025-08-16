1d9c0acd97f65ea9f4d885fb12019ae6293bdc89
Islam Naasani <islamnaasani@gmail.com>
2025-08-16 12:04:03 +0300

    feat: should break ci check

--- packages/examples/next-app/src/app/searchParams.tsx
+++ packages/examples/next-app/src/app/searchParams.tsx
@@ -3,16 +3,16 @@ import {
   createSerializer,
   parseAsInteger,
   parseAsString,
-  parseAsStringEnum
-} from 'nuqs/server'
+  parseAsStringEnum,
+} from "nuqs/server";
 
 export const searchParams = {
   page: parseAsInteger.withDefault(1),
   limit: parseAsInteger.withDefault(5),
-  order: parseAsStringEnum(['asc', 'desc']),
-  search: parseAsString.withDefault('')
-}
+  order: parseAsStringEnum(["asc", "desc"]),
+  search: parseAsString.withDefault(""),
+};
 
-export const searchParamsCache = createSearchParamsCache(searchParams)
+export const searchParamsCache = createSearchParamsCache(searchParams);
 
-export const serialize = createSerializer(searchParams)
+export const serialize = createSerializer(searchParams);
